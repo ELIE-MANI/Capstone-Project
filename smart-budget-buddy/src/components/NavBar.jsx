35d8a97ceaf768e5d6d2ce7d963cd262b8ed8506
ELIE-MANI <eliemaniraguha@gmail.com>
2025-09-30 16:09:35 +0200

    add some style on the Navbar

--- smart-budget-buddy/src/components/NavBar.jsx
+++ smart-budget-buddy/src/components/NavBar.jsx
@@ -3,12 +3,12 @@ import { Link } from "react-router";
 function NavBar() {
   return ( 
   <>
-  <nav>
-    <ul>
-  <li><Link to='/'>Dashboard</Link></li>
-  <li><Link to='/profile'>Profile</Link></li>
-  <li><Link to='/expenses'>Expenses</Link></li>
-  <li><Link to='/settings'>Settings</Link></li>
+  <nav className="text-white p-4 ">
+    <ul className="flex justify-around">
+  <li><Link to='/' className=" transition delay-300 border-b-2 border-transparent hover:border-white">Dashboard</Link></li>
+  <li><Link to='/profile' className="transition delay-300 border-b-2 border-transparent hover:border-white">Profile</Link></li>
+  <li><Link to='/expenses' className="transition delay-300 border-b-2 border-transparent hover:border-white">Expenses</Link></li>
+  <li><Link to='/settings' className="transition delay-300 border-b-2 border-transparent hover:border-white">Settings</Link></li>
   </ul>
 
   </nav>
