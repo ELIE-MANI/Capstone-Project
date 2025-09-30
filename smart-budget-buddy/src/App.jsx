7d918948b468f326f236f4d3ef36e8690291926e
ELIE-MANI <eliemaniraguha@gmail.com>
2025-09-30 15:30:48 +0200

    add routing to the app

--- smart-budget-buddy/src/App.jsx
+++ smart-budget-buddy/src/App.jsx
@@ -1,9 +1,23 @@
+import { BrowserRouter, Route, Router, Routes } from "react-router";
+import DashboardPage from "./pages/DashboardPage";
+import NavBar from "./components/NavBar";
+import ProfilePage from "./pages/ProfilePage";
+import ExpensesPage from "./pages/ExpensesPage";
+import SettingsPage from "./pages/SettingsPage";
+
 function App() {
   return ( 
     <>
-    <div>
-      <h1>Smart Buddy Budget</h1>
-    </div>
+   <BrowserRouter>
+    <NavBar/>
+   <Routes>
+    <Route path="/" element={<DashboardPage/>}/>
+    <Route path="/profile" element={<ProfilePage/>}/>
+    <Route path="/expenses" element={<ExpensesPage/>}/>
+    <Route path="/settings" element={<SettingsPage/>}/>
+   </Routes>
+ 
+   </BrowserRouter>
     </>
    );
 }
